b8df47df1f20894a5d8995cb6a8fb7f0a0711b03
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-31 21:22:14 +0500

    Add route and dummy details for the page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Companies from './components/Companies';
 import Header from './components/Header';
+import Companies from './components/Companies';
+import CompanyDetails from './components/CompanyDetails';
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
+        <Route path="/details" exact="true" element={<CompanyDetails />} />
         <Route path="/" element={<Companies />} />
       </Routes>
     </div>
--- src/components/company.js
+++ src/components/company.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import stockBg from '../images/stock_bg.jpg';
@@ -8,12 +9,14 @@ const Company = (props) => {
 
   return (
     <Card className="bg-dark text-white">
-      <Card.Img src={stockBg} alt="Card image" style={{ width: '256px' }} />
-      <Card.ImgOverlay>
-        <Card.Title>{symbol}</Card.Title>
-        <Card.Text>{name}</Card.Text>
-        <Card.Text>{price}</Card.Text>
-      </Card.ImgOverlay>
+      <NavLink to={`/${symbol}`}>
+        <Card.Img src={stockBg} alt="Card image" style={{ width: '256px' }} />
+        <Card.ImgOverlay>
+          <Card.Title>{symbol}</Card.Title>
+          <Card.Text>{name}</Card.Text>
+          <Card.Text>{price}</Card.Text>
+        </Card.ImgOverlay>
+      </NavLink>
     </Card>
   );
 };
