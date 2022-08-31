0b0f4c0a4a99028a0d961e2d39df91f38f2614a2
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-31 19:57:00 +0500

    Display data after fetching from API

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,15 +4,15 @@ import * as actions from './types';
 
 const API_KEY = '914bee6857729bc58d8f8d6ec4224f5d';
 const BASE_URL = 'https://financialmodelingprep.com/api/v3';
-const REQUESTED_URL = `${BASE_URL}/stock_market/actives?apikey=${API_KEY}}`;
+const REQUESTED_URL = `${BASE_URL}/stock_market/actives?apikey=${API_KEY}`;
 
 // Actions
 const getCompanies = createAsyncThunk(
   actions.GET_COMPANIES,
   async () => {
-    const { data } = await axios.get(REQUESTED_URL);
-    console.log(data);
-    return data;
+    const response = await axios.get(REQUESTED_URL);
+    // console.log('Response: ', response.data);
+    return response.data;
   },
 );
 
