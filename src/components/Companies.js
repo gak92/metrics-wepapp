1ddb802948ef3c6eff42360ab302fa95c46fe304
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-01 14:26:26 +0500

    Implement search functionality

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,31 +1,62 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import { getCompanies } from '../redux/actions';
 import Company from './Company';
 
 const Companies = () => {
   const dispatch = useDispatch();
   const companiesList = useSelector((state) => state.companiesReducer);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(
-    () => {
+  useEffect(() => {
+    // console.log(companiesList.length);
+    if (companiesList.length === 0) {
       dispatch(getCompanies());
-    },
-    [],
-  );
+    }
+  }, []);
+
+  const searchHandler = (event) => {
+    // console.log(event.target.value);
+    const searchVal = event.target.value;
+    if (searchVal) {
+      setSearchParams({ searchVal });
+    } else {
+      setSearchParams({});
+    }
+  };
 
   return (
-    <div className="d-flex flex-wrap gap-1">
-      {
-        companiesList.map((company) => (
-          <Company
-            key={company.symbol}
-            symbol={company.symbol}
-            name={company.name}
-            price={company.price}
-          />
-        ))
-      }
+    <div>
+      <input
+        type="text"
+        placeholder="Search company by Name ..."
+        value={searchParams.keyword}
+        onChange={searchHandler}
+      />
+      <div className="d-flex flex-wrap gap-1">
+        {
+          companiesList.filter((company) => {
+            const companyName = company.name.toLowerCase();
+            const searchQuery = searchParams.get('searchVal');
+            // console.log('Search Query: ', !searchQuery);
+            if (searchQuery) {
+              // console.log(companyName.includes(searchQuery));
+              return companyName.includes(searchQuery);
+            }
+            // console.log('All true');
+            return true;
+          })
+            .map((company) => (
+              <Company
+                key={company.symbol}
+                symbol={company.symbol}
+                name={company.name}
+                price={company.price}
+              />
+            ))
+        }
+      </div>
     </div>
   );
 };
